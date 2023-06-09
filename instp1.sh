8335b46d70f831c1ba461d2c1308d08fcf51ae53
Flavio Del Mestre <flavio@perunis.ch>
2023-06-09 14:57:56 +0200

    Update instp1.sh

--- instp1.sh
+++ instp1.sh
@@ -33,5 +33,6 @@ rm README.md
 
 wget -O virtualmin-install.sh https://raw.githubusercontent.com/virtualmin/virtualmin-install/master/virtualmin-install.sh
 sh virtualmin-install.sh -y
+rm virtualmin-install.sh
 reboot
 
