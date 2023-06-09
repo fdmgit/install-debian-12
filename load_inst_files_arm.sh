32a4dd6f6af6195492d6ad8a8a1b2e580566d4fc
Flavio Del Mestre <flavio@perunis.ch>
2023-06-09 14:57:09 +0200

    Update load_inst_files_arm.sh

--- load_inst_files_arm.sh
+++ load_inst_files_arm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/bashrc.ini
-wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instubp1arm.sh
-wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instubp2arm.sh
+wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instp1arm.sh
+wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instp2arm.sh
 
 cp bashrc.ini /root/.bashrc
 rm /root/bashrc.ini
 
-chmod +x /root/instubp1arm.sh
-chmod +x /root/instubp2arm.sh
+chmod +x /root/instp1arm.sh
+chmod +x /root/instp2arm.sh
