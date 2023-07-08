14c00b25a2c1de706e50a4e49f0bad8810422735
Flavio Del Mestre <flavio@perunis.ch>
2023-07-08 11:39:52 +0200

    Update instp2arm.sh

--- instp2arm.sh
+++ instp2arm.sh
@@ -394,11 +394,12 @@ END
 #### new fail2ban and jail
 ###################################
 
-wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/jaildeb12.local
+wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/jail-deb12.local
 cd /etc/fail2ban
 mv jail.local jail.local.orig
-cp /root/jaildeb12.local jail.local
-rm /root/jaildeb12.local
+cp /root/jail-deb12.local jail.local
+rm /root/jail-deb12.local
+touch /var/log/auth.log
 
 
 cd /root
