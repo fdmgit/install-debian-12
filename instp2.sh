e69ec9e34ca39de884ebbc8406b3aa3187c41882
Flavio Del Mestre <flavio@perunis.ch>
2023-08-13 12:53:13 +0200

    Update instp2.sh

--- instp2.sh
+++ instp2.sh
@@ -324,6 +324,8 @@ cd /root
 #### Install Redis Server
 ##############################
 
+curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 apt update
 apt install redis -y
 systemctl enable --now redis-server
