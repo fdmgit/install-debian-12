be7024eaf42a7d5d148cf9ef6f545b4457d85ca4
Flavio Del Mestre <flavio@perunis.ch>
2023-08-14 16:36:26 +0200

    Update load_inst_files_arm.sh

--- load_inst_files_arm.sh
+++ load_inst_files_arm.sh
@@ -10,4 +10,17 @@ rm /root/bashrc.ini
 chmod +x /root/instp1arm.sh
 chmod +x /root/instp2arm.sh
 
+###################################
+#### Setup root key file
+###################################
+
+if [ -d /root/.ssh ]; then 
+    echo ".ssh exists"
+else
+    mkdir /root/.ssh
+fi
+
+cd /root/.ssh
+wget https://raw.githubusercontent.com/fdmgit/virtualmin/main/authorized_keys
+
 reboot
