550a0a3b48b8ee984d7543bd44524d824efe223f
Flavio Del Mestre <flavio@perunis.ch>
2023-08-29 11:26:44 +0200

    Update raspbase.sh

--- raspbase.sh
+++ raspbase.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/bashrc.ini
+wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instp1arm.sh
+wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instp2arm.sh
 
 cp bashrc.ini /root/.bashrc
 rm /root/bashrc.ini
+
+chmod +x /root/instp1arm.sh
+chmod +x /root/instp2arm.sh
 
 ###################################
 #### Setup root key file
