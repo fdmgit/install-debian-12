c56f5046626f935a8d4b5e8671a61224e54f5917
Flavio Del Mestre <flavio@perunis.ch>
2023-12-25 13:11:13 +0100

    Update instp1arm.sh

--- instp1arm.sh
+++ instp1arm.sh
@@ -10,10 +10,6 @@ apt upgrade -y
 apt install plocate sntp ntpdate software-properties-common curl apt-transport-https -y
 timedatectl set-timezone Europe/Zurich
 hostnamectl set-hostname $2  # set hostname
-echo "deb http://deb.debian.org/debian/ bookworm-backports main" | tee -a /etc/apt/sources.list
-echo "deb-src http://deb.debian.org/debian/ bookworm-backports main" | tee -a /etc/apt/sources.list
-echo "deb http://deb.debian.org/debian/ experimental main" | tee -a /etc/apt/sources.list
-echo "deb-src http://deb.debian.org/debian/ experimental main" | tee -a /etc/apt/sources.list
 
 apt update 
 
