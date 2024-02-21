4bd118fe72dee7cc203d8bd6b42104bab440b235
Flavio Del Mestre <flavio@perunis.ch>
2024-02-21 10:00:28 +0100

    Create instphp82.sh

---
+++ instphp82.sh
@@ -0,0 +1,6 @@
+
+
+apt install php8.2-bcmath php8.2-bz2 php8.2-cgi php8.2-curl php8.2-dba php8.2-fpm -y
+apt install php8.2-gd php8.2-gmp php8.2-igbinary php8.2-imagick php8.2-imap php8.2-intl php8.2-ldap php8.2-mbstring -y
+apt install php8.2-mysql php8.2-odbc php8.2-opcache php8.2-pspell php8.2-readline -y
+apt install php8.2-redis php8.2-soap php8.2-sqlite3 php8.2-tidy php8.2-xml php8.2-xmlrpc php8.2-xsl php8.2-zip -y
