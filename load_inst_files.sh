77cbed07e51e07d9fdbcf9666480d66fae9928ad
Flavio Del Mestre <flavio@perunis.ch>
2024-04-20 09:22:31 +0200

    Update load_inst_files.sh

--- load_inst_files.sh
+++ load_inst_files.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Define colors
+RED='\033[0;31m'
+LRED='\033[0;91m'
+BLUE='\033[0;34m'
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+# Retrieve the IP address
+ip_address=$(hostname -I | awk '{print $1}')
+
+
 wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/bashrc.ini
 wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instp1.sh
 wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/instp2.sh
@@ -51,5 +63,12 @@ sed -i "s|\#Port 22|Port 49153|g" /etc/ssh/sshd_config
 sed -i "s|\#MaxAuthTries 6|MaxAuthTries 4|g" /etc/ssh/sshd_config
 sed -i "s|X11Forwarding yes|X11Forwarding no|g" /etc/ssh/sshd_config
 sed -i "s|session    required     pam_env.so user_readenv=1 envfile=/etc/default/locale|session    required     pam_env.so envfile=/etc/default/locale|g" /etc/pam.d/sshd
+
+# Closing message
+echo ""
+echo -e "${YELLOW}ATTENTION\\n"
+echo -e "${GREEN}The port for SSH has changed. To login use the following comand:\\n"
+echo -e "        ssh root@${ip_address} -p 49153${NC}\\n"
+
 
 reboot
