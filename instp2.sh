5219e444e360f1d583d39ec34cb1b8bd9e15f9ec
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2024-05-06 16:38:00 +0200

    Update instp2.sh

--- instp2.sh
+++ instp2.sh
@@ -464,9 +464,9 @@ END
 
 cd /root
 
-wget -O fail2ban_1.1.0-1.deb  https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb
-dpkg -i --force-confnew fail2ban_1.1.0-1.deb
-rm fail2ban_1.1.0-1.deb
+wget -O fail2ban_newest.deb  https://github.com/fail2ban/fail2ban/releases/download/1.1.0/fail2ban_1.1.0-1.upstream1_all.deb
+dpkg -i --force-confnew fail2ban_newest.deb
+rm fail2ban_newest.deb
 
 wget https://raw.githubusercontent.com/fdmgit/install-debian-12/main/jail-deb12.local
 cd /etc/fail2ban
