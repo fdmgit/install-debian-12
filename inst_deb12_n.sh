e9c63d152926cd241d8b81c3ed90f9700ea3604a
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2024-12-28 13:50:25 +0100

    Update inst_deb12_n.sh

--- inst_deb12_n.sh
+++ inst_deb12_n.sh
@@ -1131,7 +1131,6 @@ inst_php84               # function
 enable_apache_mod        # function
 inst_redis               # function
 inst_virtualmin_config   # function
-inst_logo_styles         # function
 inst_f2b                 # function
 inst_pwgen               # function
 inst_mc                  # function
@@ -1142,6 +1141,7 @@ inst_bip                 # function
 post_inst                # function
 inst_motd                # function
 inst_composer            # function
+inst_logo_styles         # function
 closing_msg              # function
 
 reboot
