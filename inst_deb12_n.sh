020a6f5a2804d4cf16baa046d9e2dfa2d202ad0f
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-01-28 23:06:50 +0100

    Update inst_deb12_n.sh

--- inst_deb12_n.sh
+++ inst_deb12_n.sh
@@ -4,26 +4,26 @@
 #             Var / Const Definition             #
 ##################################################
 
-okinput=true
+#okinput=true
 
 NC=$(echo -en '\001\033[0m\002')
 RED=$(echo -en '\001\033[00;31m\002')
 GREEN=$(echo -en '\001\033[00;32m\002')
 YELLOW=$(echo -en '\001\033[00;33m\002')
-BLUE=$(echo -en '\001\033[00;34m\002')
-MAGENTA=$(echo -en '\001\033[00;35m\002')
-PURPLE=$(echo -en '\001\033[00;35m\002')
-CYAN=$(echo -en '\001\033[00;36m\002')
-WHITE=$(echo -en '\001\033[01;37m\002')
+#BLUE=$(echo -en '\001\033[00;34m\002')
+#MAGENTA=$(echo -en '\001\033[00;35m\002')
+#PURPLE=$(echo -en '\001\033[00;35m\002')
+#CYAN=$(echo -en '\001\033[00;36m\002')
+#WHITE=$(echo -en '\001\033[01;37m\002')
 
-LIGHTGRAY=$(echo -en '\001\033[00;37m\002')
-LRED=$(echo -en '\001\033[01;31m\002')
-LGREEN=$(echo -en '\001\033[01;32m\002')
-LYELLOW=$(echo -en '\001\033[01;33m\002')
-LBLUE=$(echo -en '\001\033[01;34m\002')
-LMAGENTA=$(echo -en '\001\033[01;35m\002')
-LPURPLE=$(echo -en '\001\033[01;35m\002')
-LCYAN=$(echo -en '\001\033[01;36m\002')
+#LIGHTGRAY=$(echo -en '\001\033[00;37m\002')
+#LRED=$(echo -en '\001\033[01;31m\002')
+#LGREEN=$(echo -en '\001\033[01;32m\002')
+#LYELLOW=$(echo -en '\001\033[01;33m\002')
+#LBLUE=$(echo -en '\001\033[01;34m\002')
+#LMAGENTA=$(echo -en '\001\033[01;35m\002')
+#LPURPLE=$(echo -en '\001\033[01;35m\002')
+#LCYAN=$(echo -en '\001\033[01;36m\002')
 
 
 ##################################################
@@ -144,7 +144,7 @@ function closing_msg () {
 	echo -e "${GREEN} Webmin page is reachable by entering:\\n"
         echo -e "${CYAN}        https://${host_name}:10000"
         echo -e "${NC}\\n"
-	echo -e "End Time:" $(date +"%d.%m.%Y %T")
+	echo -e "End Time:" "$(date +"%d.%m.%Y %T")"
         echo ""
 	echo ""
 }
