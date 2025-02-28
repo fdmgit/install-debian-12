3eb96a4c8b1e3ee33e7ff8b86a5233ae93925723
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-02-28 09:48:44 +0100

    updated kernel

--- inst_deb12_n_arm.sh
+++ inst_deb12_n_arm.sh
@@ -399,7 +399,7 @@ function inst_kernel() {
     #### Install new Linux Kernel
     ##############################
 
-    apt install linux-image-6.12.9+bpo-arm64 -y
+    apt install linux-image-6.12.12+bpo-arm64 -y
     # apt install linux-headers-6.12.9+bpo-arm64 -y  # development
 
 }
