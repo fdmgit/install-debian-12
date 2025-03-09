3068bd4b94b187d04bac6a8d9f24b05b5bd79805
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-03-09 15:11:15 +0100

    Update inst_deb12_n2.sh

--- inst_deb12_n2.sh
+++ inst_deb12_n2.sh
@@ -420,9 +420,6 @@ function inst_sury_repo() {
     #### Fix as https://packages.sury.org/php/README.txt has an error
     sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
     ####
-
-    # wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-    # echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
     
     apt update
     apt upgrade -y
