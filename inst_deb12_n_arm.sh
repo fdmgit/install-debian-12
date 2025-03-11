d9d0a6545ad3ad7b2d059a7fd2a3dd904dc5fdb8
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-03-11 14:40:40 +0100

    Update inst_deb12_n_arm.sh

--- inst_deb12_n_arm.sh
+++ inst_deb12_n_arm.sh
@@ -411,9 +411,14 @@ function inst_sury_repo() {
     ##############################
 
     apt -y install lsb-release apt-transport-https ca-certificates
+    
     echo | curl -sSL https://packages.sury.org/apache2/README.txt | sudo bash -xe
-    wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+
+    echo | curl -sSL https://packages.sury.org/php/README.txt | sudo bash -xe
+    
+    #wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+    #echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php.list
+    
     apt update
     apt upgrade -y
 
