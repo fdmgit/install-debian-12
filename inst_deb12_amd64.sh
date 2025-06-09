2b82f5abd75942392ca62b4615e06d547c39227b
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2025-06-09 17:25:36 +0200

    Update inst_deb12_amd64.sh

--- inst_deb12_amd64.sh
+++ inst_deb12_amd64.sh
@@ -889,7 +889,7 @@ function inst_gat() {
     ###################################
 
     cd /usr/local/bin || exit
-    wget https://github.com/koki-develop/gat/releases/download/v0.19.3/gat_Linux_x86_64.tar.gz
+    wget https://github.com/koki-develop/gat/releases/download/v0.24.1/gat_Linux_x86_64.tar.gz
     tar -xvzf gat_Linux_x86_64.tar.gz
     chown root:root gat
     chmod +x gat
